e163e24782da8242efa60314f22a0e7832de2ded
spencerXXX-WB <activegreatmind@gmail.com>
2025-01-15 10:21:24 +0000

    Removal of links

--- client/src/App.js
+++ client/src/App.js
@@ -42,14 +42,9 @@ function App() {
     return (
         <div className="app">
             <div className="navbar">
-                <div className="brand">DevOps Shack</div>
+                <div className="brand">DevOps</div>
                 <div className="nav-links">
-                    <a className="home" href="/">Home</a>
-                    <a className="youtube" href="https://www.youtube.com/@devopsshack/videos">YouTube</a>
-                    <a className="courses" href="https://www.devopsshack.com/">Courses</a>
-                    <a className="telegram" href="https://t.me/+9roGPjX1YI42Yzdl">Telegram</a>
-                    <a className="instagram" href="https://www.instagram.com/devopsshack/">Instagram</a>
-                </div>
+                                  </div>
             </div>
             <h1>DevOps Shack User Management App</h1>
             <div className="form-container">
